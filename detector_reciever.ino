c1b35f62e23760c70b195d8d4da71b583d7b49cc
vertiass <128167950+vertiass@users.noreply.github.com>
2024-07-02 12:09:39 -0400

    changes

--- detector_reciever.ino
+++ detector_reciever.ino
@@ -1,42 +1,78 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <esp_now.h>
 #include <WiFi.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 typedef struct struct_message {
-  float voltage;
-  float dB;
+  float decibels;
 } struct_message;
 
 struct_message myData;
 
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.println();
-  Serial.println("<<<<< Receive Data:");
   Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("Voltage: ");
-  Serial.println(myData.voltage);
-  Serial.print("dB: ");
-  Serial.println(myData.dB);
-  Serial.println("<<<<<");
+  Serial.print("Decibels: ");
+  Serial.println(myData.decibels);
+  
+  display.clearDisplay();
+  
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print("Nivel de Sonido: ");
+  display.setTextSize(2);
+  display.setCursor(0, 16);
+  display.print(myData.decibels, 1);
+  display.print(" dB");
+
+  String soundLevel;
+  if (myData.decibels < 60) {
+    soundLevel = "Normal";
+  } else if (myData.decibels < 80) {
+    soundLevel = "Medio";
+  } else {
+    soundLevel = "Alto";
+  }
+
+  display.setTextSize(1);
+  display.setCursor(0, 48);
+  display.print("Nivel: ");
+  display.print(soundLevel);
+
+  display.display();
 }
 
 void setup() {
   Serial.begin(115200);
 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+
   WiFi.mode(WIFI_STA);
+  Serial.println("ESP32 en modo estación");
+
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
+  Serial.println("ESP-NOW inicializado correctamente");
+
 
   esp_now_register_recv_cb(OnDataRecv);
-
-  /*--------Por hacer-----------
-  Enviar los datos via HTTP a la pagina web, de manera que se puedan
-  visualizar los decibeles.
-  */
 }
 
 void loop() {
--- detector_sender.ino
+++ detector_sender.ino
@@ -2,15 +2,19 @@
 #include <WiFi.h>
 
 #define SOUND_SENSOR_PIN 34
+const int sampleWindow = 5;
 
 uint8_t broadcastAddress[] = {0xA0, 0xB7, 0x65, 0xDC, 0x14, 0x94};
 
 typedef struct struct_message {
-  float voltage;
-  float dB;
+  float decibels;
 } struct_message;
 
 struct_message myData;
+
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
@@ -20,44 +24,57 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 void setup() {
   Serial.begin(115200);
 
-  pinMode(SOUND_SENSOR_PIN, INPUT);
-
   WiFi.mode(WIFI_STA);
+  Serial.println("ESP32 en modo estación");
 
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-
+  Serial.println("ESP-NOW inicializado correctamente");
+  
   esp_now_register_send_cb(OnDataSent);
 
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
-  peerInfo.channel = 0;
+  peerInfo.channel = 0;  
   peerInfo.encrypt = false;
-
+  
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
+  Serial.println("Peer agregado correctamente");
+
+  pinMode(SOUND_SENSOR_PIN, INPUT);
 }
 
 void loop() {
-  // Leer el valor analogo desde el sensor de sonido
-  int sensorValue = analogRead(SOUND_SENSOR_PIN);
+  unsigned long startMillis = millis();
+  float peakToPeak = 0;                  
 
-  // Convertir el valor analogo a voltaje
-  float voltage = sensorValue * (3.3 / 4095.0);
+  unsigned int signalMax = 0;           
+  unsigned int signalMin = 4095;         
 
-  // Convertir el voltaje a decibeles
-  float dB = 20 * log10(voltage / 0.00631); // 0.00631 es un valor de voltaje de referencia, necesita calibracion
+  while (millis() - startMillis < sampleWindow) {
+    int sample = analogRead(SOUND_SENSOR_PIN);
+    if (sample < 4095) {                        
+      if (sample > signalMax) {
+        signalMax = sample;
+      }
+      if (sample < signalMin) {
+        signalMin = sample;
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = amplitud peak-to-peak
+  
+  // Convertir peak-to-peak a decibeles usando un mapeo
+  float decibels = mapFloat(peakToPeak, 20, 900, 49.5, 90); 
 
-  Serial.println(dB);
-
-  myData.voltage = voltage;
-  myData.dB = dB;
-
-  // Enviar los datos mediante protocolo ESP-NOW
+  myData.decibels = decibels;
+  
+  // Enviar datos al receptor
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 
   if (result == ESP_OK) {
@@ -66,5 +83,5 @@ void loop() {
     Serial.println("Error sending the data");
   }
 
-  delay(250);
+  delay(500);
 }
